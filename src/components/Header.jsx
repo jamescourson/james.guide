7745762393afd33d3aff1523be353b444a94765f
James <jcourson@infobate.com>
2024-03-30 16:25:35 -0400

    styling, add clock widget

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,32 @@
 import { Link } from 'react-router-dom';
 import Logo from '../assets/logo.png';
+import { useEffect, useMemo, useState } from 'react';
 
 const headerStyles = {
   display: 'flex',
-  flexFlow: 'row no-wrap',
-  justifyContent: 'center',
-  alignItems: 'baseline'
+  flexDirection: 'column',
+  alignItems: 'center'
 }
 
 const logoStyles = {
   maxHeight: '3em'
 }
 
-const Header = () => (
-  <header style={headerStyles}>
-    <Link to="/">
-      <img src={Logo} alt="James' Guide" style={logoStyles} />
-    </Link>
-  </header>
-);
+const Header = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  
+  useEffect(() => {
+    window.setTimeout(() => setCurrentTime(new Date()), 1000);
+  });
+
+  return (
+    <header style={headerStyles}>
+      <Link to="/">
+        <img src={Logo} alt="James' Guide" style={logoStyles} />
+      </Link>
+      <span id="clock">{currentTime.toLocaleTimeString()}</span>
+    </header>
+  );
+}
 
 export default Header;
