31d9dc8949b02daa376193dcd34e1de0f6967176
James <jcourson@infobate.com>
2024-07-30 12:43:20 -0400

    component directories, style updates

--- src/pages/Directory/Directory.jsx
+++ src/pages/Directory/Directory.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
+import './directory.css';
+
 
 const Directory = () => (
   <ul>
+    <li><Link to="/g/ch">Chess</Link></li>
     <li><Link to="/g/mu">Music</Link></li>
     <li><Link to="/g/sd">Software Development</Link></li>
     <li><Link to="/g/wd">Web Development</Link></li>
