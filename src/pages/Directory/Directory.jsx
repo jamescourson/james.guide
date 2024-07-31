60df6cb511c69ead9ece8c4c35234d6f2e39918b
James Courson <brokencontigo@gmail.com>
2024-07-31 11:38:18 -0400

    add stats, layout, dynamic links from list

--- src/pages/Directory/Directory.jsx
+++ src/pages/Directory/Directory.jsx
@@ -2,14 +2,68 @@ import { Link } from 'react-router-dom';
 import './directory.css';
 
 
-const Directory = () => (
-  <ul>
-    <li><Link to="/g/ch">Chess</Link></li>
-    <li><Link to="/g/mu">Music</Link></li>
-    <li><Link to="/g/sd">Software Development</Link></li>
-    <li><Link to="/g/wd">Web Development</Link></li>
-    <li><Link to="/g/ww">Woodworking</Link></li>
-  </ul>
-);
+const DirectoryListing = ({ name, abbr }) => {
+  const resourceCount = 'X'; // fetch from api
+
+  return (
+    <li style={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      gap: '1em'
+    }}>
+      <Link to={`/g/${abbr}`}>{name}</Link>
+      <small>{resourceCount} resources</small>
+    </li>
+  );
+}
+
+const Directory = () => {
+  const directories = [
+    {
+      name: 'Calisthenics',
+      abbr: 'ca'
+    },
+    {
+      name: 'Chess',
+      abbr: 'ch'
+    },
+    {
+      name: 'Disc Golf',
+      abbr: 'dg'
+    },
+    {
+      name: 'Music',
+      abbr: 'mu'
+    },
+    {
+      name: 'Origami',
+      abbr: 'or'
+    },
+    {
+      name: 'Software Development',
+      abbr: 'sd'
+    },
+    {
+      name: 'Web Development',
+      abbr: 'wd'
+    },
+    {
+      name: 'Woodworking',
+      abbr: 'ww'
+    },
+  ]
+  const totalResources = 'X'; // fetch from api
+
+  return (
+    <>
+      <h1>Directory</h1>
+      <ul>
+        {directories.map(({ name, abbr }, i) => (<DirectoryListing name={name} abbr={abbr} key={i}></DirectoryListing>))}
+      </ul>
+      <small>{directories.length} directories | {totalResources} total resources</small>
+    </>
+  );
+}
 
 export default Directory;
