63b3a076488dfa7813fc29f4887b0a8ee1c8a846
James Courson <brokencontigo@gmail.com>
2024-07-31 21:08:53 -0400

    wording

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,9 +8,9 @@ const About = () => (
   <article style={aboutStyles}>
     <h1>Thanks for visiting James' Guide!</h1>
     <p>
-      I've always been a bit of a collector when it comes to things I'm passionate about - so it's no surprise that I collect quality webpages too, right?
+      I've always been a bit of a collector when it comes to the things I'm passionate about - so it's no surprise that I collect webpages too, right?
       <br />
-      This website serves as a repository for <b>practical and high-quality resources</b> I've found for my hobbies and interests. <small>There's also a <Link to="/j">dumb blog</Link>!</small>
+      This website serves as a repository for <b>practical and high-quality resources</b> I've found for my hobbies and interests. <small>(There's also a <Link to="/j">dumb blog</Link>!)</small>
       <br />
       This website aims to make these resources available for other like-minded people that wish to improve their knowledge and skills.
       <br />
