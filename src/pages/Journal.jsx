17a7db6e3e6118782baf5e78822d9d43179fb7c1
James Courson <brokencontigo@gmail.com>
2024-07-31 20:43:03 -0400

    Blog -> Journal

---
+++ src/pages/Journal.jsx
@@ -0,0 +1,9 @@
+const Journal = () => (
+  <>
+    <h1>James' Guide to Life</h1>
+    <p>A journal about my experiences as a 20-something fool</p>
+    <small>Coming soon</small>
+  </>
+);
+
+export default Journal;  
