926bdbd9d9bd3763c377b7ae58e7fdb622fcdd49
James Courson <brokencontigo@gmail.com>
2024-08-01 11:49:01 -0400

    add guide component

---
+++ src/pages/Guide/Guide.jsx
@@ -0,0 +1,35 @@
+import { useMemo } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import guides from '../../data/guides.json';
+import resources from '../../data/resources.json';
+
+import './guide.css';
+
+
+const Resource = ({ data }) => {
+  const { title, description, url } = data;
+
+  return (
+    <article>
+      <h2><Link to={url}>{title}</Link></h2>
+      <p>{description}</p>
+    </article>
+  );
+}
+
+const Guide = () => {
+  const { abbr } = useParams();
+  const guideData = useMemo(() => guides.filter(guide => guide.abbr === abbr));
+  const filteredResources = useMemo(() => resources.filter(resource => resource.guide === abbr));
+
+  return (
+    <>
+      <h1>{guideData.name}</h1>
+      <ul>
+        {filteredResources.map((resource, key) => <Resource data={resource} key={key} />)}
+      </ul>
+    </>
+  );
+}
+
+export default Guide;
