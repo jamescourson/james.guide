147ea57906fbae19905c398667f767dea12edc22
James Courson <brokencontigo@gmail.com>
2024-08-04 00:20:34 -0400

    add coming soon list, formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,17 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <>
     <h1>Welcome to James' Guide!</h1>
+    <small>Live clock updating by second</small>
     <article>
-      <p>
-        If it's your first time here, check out the <Link to="/d">Directory</Link>!
-      </p>
-
-      <h1>About</h1>
+      <h2>About</h2>
       <p>
         I've always been a bit of a collector - so it's no surprise that I collect webpages too, right?
         <br />
         This website serves as a repository for <b>practical and high-quality internet resources</b> that I've found for my hobbies and interests. <small>(There's also a <Link to="/j/">dumb blog</Link>!)</small>
         <br />
         My goal is to make these resources available for other like-minded people that wish to further improve their knowledge and skills.
+        <br />
+        If it's your first time here, check out the <Link to="/d">Directory</Link>!
       </p>
 
       <p>
@@ -25,6 +24,14 @@ const Home = () => (
         <br />
         Check out the <Link to="https://github.com/jamescourson/james.guide" target="_blank">source code</Link>!
       </p>
+
+      <h2>Coming Soon</h2>
+      <p>Here's some features to look forward to soon:</p>
+      <ul>
+        <li>Suggesting resources</li>
+        <li>Commending resources</li>
+        <li>Getting that clock up above working</li>
+      </ul>
     </article>
   </>
 );
