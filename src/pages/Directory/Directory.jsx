efb61b416622873788ee53f60b8c1bd1c62cb1e8
jamescourson <james@james.guide>
2024-11-18 23:43:08 -0500

    new directory view

--- src/pages/Directory/Directory.jsx
+++ src/pages/Directory/Directory.jsx
@@ -1,31 +1,23 @@
-import { Link } from 'react-router-dom';
 import './directory.css';
+import ResourceTable from '../../components/ResourceTable/ResourceTable';
+
 import guides from '../../data/guides.json';
 import resources from '../../data/resources.json';
 
 
-const GuideListing = ({ name, abbr }) => {
-  const resourceCount = resources.filter(resource => resource.guide === abbr).length;
-
-  return (
-    <li>
-      <Link to={`/g/${abbr}`}>{name}</Link>
-      <small>{resourceCount} resources</small>
-    </li>
-  );
-}
-
 const Directory = () => {
   const totalValidResources = resources.filter(resource => resource.id && resource.id > 0).length;
+  const lastUpdated = () => {
+    const newDate = new Date();
+    return `${newDate.toLocaleDateString()}, ${newDate.toLocaleTimeString()}`;
+  }
 
   return (
-    <>
+    <article>
       <h1>Directory</h1>
-      <small>{guides.length} guides | {totalValidResources} resources</small>
-      <ul>
-        {guides.map(({ name, abbr }, i) => (<GuideListing name={name} abbr={abbr} key={i} />))}
-      </ul>
-    </>
+      <small>{guides.length} guides | {totalValidResources} resources | last updated {lastUpdated()}</small>
+      <ResourceTable />
+    </article>
   );
 }
 
