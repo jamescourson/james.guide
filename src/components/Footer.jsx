bc172dfb202a1c4009f627c99caa4c223cacaff0
jamescourson <james@james.guide>
2024-11-18 22:26:39 -0500

    add knight, alignment, sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 
 const footerStyles = {
-  padding: '.5em',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '.5em',
+  position: 'absolute',
+  bottom: 0,
+  right: 0,
+  left: 0,
   textAlign: 'center',
   color: 'ivory',
-  backgroundColor: 'rgba(0, 0, 0, .1)'
+  backgroundColor: 'rgba(0, 0, 0, .1)',
+  padding: '.5em',
 }
 
 const Footer = () => (
   <footer style={footerStyles}>
     <small>&copy; James Courson 2024 &#187; <Link to="https://courson.dev" target="_blank">courson.dev</Link></small>
+    <small>&#9822;</small>
   </footer>
 );
 
