7ffe5a7af699e797ccef8b55230d2cb837a714c6
jamescourson <james@james.guide>
2024-11-18 22:39:53 -0500

    update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,15 @@ const Home = () => (
     <article>
       <h2>About</h2>
       <p>
-        I've always been a bit of a collector - so it's no surprise that I collect webpages too, right?
-        <br />
-        This website serves as a repository for <b>practical and high-quality internet resources</b> that I've found for my hobbies and interests. <small>(There's also a <Link to="/j/">dumb blog</Link>!)</small>
+        This website serves as a repository for <b>practical and high-quality internet resources</b> that I've found and frequently use for my hobbies and interests. <small>(There's also a <Link to="/j/">dumb blog</Link>!)</small>
         <br />
         My goal is to make these resources available for other like-minded people that wish to further improve their knowledge and skills.
         <br />
-        If it's your first time here, check out the <Link to="/d">Directory</Link>!
+        If it's your first time here, take a look at the <Link to="/d">Directory</Link> to get started!
       </p>
 
       <p>
-        James' Guide is built on <b>React</b> and runs on <b>GitHub Pages</b>.
+        James' Guide is built on <b>React and react-router</b> and is hosted on <b>GitHub Pages</b>.
         <br />
         Data is stored statically in <code>src/data/</code> and rendered from JSON.
         <br />
