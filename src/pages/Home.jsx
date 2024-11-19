48e0ee423f0341e037e1405fd5f8e9fc3068ec02
jamescourson <james@james.guide>
2024-11-19 01:38:13 -0500

    update phrasing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => (
       <p>
         This website serves as a repository for <b>practical and high-quality internet resources</b> that I've found and frequently use for my hobbies and interests. <small>(There's also a <Link to="/j/">dumb blog</Link>!)</small>
         <br />
-        My goal is to make these resources available for other like-minded people that wish to further improve their knowledge and skills.
+        My goal is to make these resources available for any like-minded people that happen to stumble upon this website.
         <br />
         If it's your first time here, take a look at the <Link to="/d">Directory</Link> to get started!
       </p>
