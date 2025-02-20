89c7ed515d446b8f0747e936c10c3f8459850693
James Courson <brokencontigo@gmail.com>
2025-02-20 11:01:01 -0500

    update copyright date

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.css';
 
 const Footer = () => (
   <footer>
-    <small>&copy; James Courson 2024 &#187; <Link to="https://courson.dev" target="_blank">courson.dev</Link></small>
+    <small>&copy; James Courson 2025 &#187; <Link to="https://courson.dev" target="_blank">courson.dev</Link></small>
     <small>&#9822;</small>
   </footer>
 );
