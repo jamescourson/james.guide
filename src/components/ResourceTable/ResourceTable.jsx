4dd90bd7509cbc5460ccd9735bcccc956a295251
jamescourson <brokencontigo@gmail.com>
2025-11-10 22:47:27 -0500

    refactor, add filter

--- src/components/ResourceTable/ResourceTable.jsx
+++ src/components/ResourceTable/ResourceTable.jsx
@@ -1,45 +1,39 @@
 import { useParams, Link } from 'react-router-dom';
 import { useMemo } from 'react';
 
-import resources from '../../data/resources.json';
-
 import './resourceTable.css';
 
+const groupBy = (arr, property) =>
+  arr.reduce((memo, x) => {
+    if (!memo[x[property]]) {
+      memo[x[property]] = [];
+    }
+    memo[x[property]].push(x);
+    return memo;
+  }, {});
 
-const alphabetize = (arr) => 
-  arr.slice().sort((resourceA, resourceB) => 
-    (resourceA.name < resourceB.name ? -1 :
-      (resourceA.name > resourceB.name ? 1 : 0))
-  );
+const TagChip = ({ tag }) => (
+  <small>{tag}</small>
+);
 
 const Resource = ({ data }) => {
-  const { name, description, url } = data;
+  const { name, description, url, tags = [] } = data;
 
   return (
     <li className="list-row">
       <Link to={url} target="_blank">
         <h3>{name}</h3>
         <p>{description}</p>
+        <div>{tags.sort().map((_tag, i) =>
+          <TagChip tag={_tag} key={i} />)}
+        </div>
       </Link>
     </li>
   );
 }
 
-const ResourceTable = () => {  
-  const { abbr } = useParams();
-  const filteredResources = useMemo(() => resources.filter(resource => resource.guide === abbr));
-  const alphabetizedResources = useMemo(() => alphabetize(filteredResources));
-
-  const groupBy = (arr, property) =>
-    arr.reduce((memo, x) => {
-      if (!memo[x[property]]) {
-        memo[x[property]] = [];
-      }
-      memo[x[property]].push(x);
-      return memo;
-    }, {});
-  
-  const categorizedResources = useMemo(() => groupBy(alphabetizedResources, 'category'));
+const ResourceTable = ({ resources }) => {
+  const categorizedResources = useMemo(() => groupBy(resources, 'category'));
   const alphabetizedCategories = Object.keys(categorizedResources).sort();
 
   return (
@@ -50,10 +44,18 @@ const ResourceTable = () => {
           <li key={index}>
             <span className="resource-list-header">
               <h2>&gt; {formattedCategoryName}</h2>
-              <small>{categorizedResources[category].length} {categorizedResources[category].length === 1 ? 'resource' : 'resources'}</small>
+              <small>
+                {categorizedResources[category].length} {categorizedResources[category].length === 1
+                  ? 'resource'
+                  : 'resources'
+                }
+              </small>
             </span>
             <ul id="resource-list">
-              {categorizedResources[category].map((resource, key) => <Resource data={resource} key={key} />)}
+              {categorizedResources[category]
+                .map((resource, key) => 
+                  <Resource data={resource} key={key} />)
+              }
             </ul>
           </li>
         );
--- src/pages/Guide/Guide.jsx
+++ src/pages/Guide/Guide.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import guides from '../../data/guides.json';
 import resources from '../../data/resources.json';
@@ -8,24 +8,70 @@ import ResourceTable from '../../components/ResourceTable/ResourceTable';
 import './guide.css';
 
 
+const alphabetizeResources = (arr) => 
+  arr.slice().sort((resourceA, resourceB) => 
+    (resourceA.name < resourceB.name ? -1 :
+    (resourceA.name > resourceB.name ? 1 :
+  0 ))
+);
+
 const ReturnButton = () => (
   <Link to="/d/" id="return"><span>&#8629;</span>&nbsp;Return</Link>
 );
 
+const FilterSelect = ({ tags, setFilter }) => (
+  tags ? (
+      <select
+        onChange={e => setFilter(e.target.value)}
+        id="guide-filter-select"
+        defaultValue=''
+      >
+        <option value={''}></option>
+        {tags.map((tag, i) =>
+          <option value={tag} key={i}>{tag}</option>)}
+      </select>
+  ) : null
+)
+
+
 const Guide = () => {
   const { abbr } = useParams();
+  
   const guideData = useMemo(() => guides.find(guide => guide.abbr === abbr));
-  const resourceCount = useMemo(() => resources.filter(resource => resource.guide === abbr).length);
+  const guideResources = useMemo(() => resources.filter(resource => resource.guide === abbr));
+  const tags = useMemo(() => guideResources
+  .map(resource => resource.tags ? resource.tags : '') // reduce to tags
+  .reduce((acc, { tags }) => { // reduce to unique tags
+    for (tag in tags)
+      if (!acc.includes(tag))
+        acc.push(tag);
+      return acc;
+    })
+    .sort() // alphabetize
+  );
+  
+  const [filter, setFilter] = useState('');
+  const filteredResources = useMemo(() =>
+    alphabetizeResources(guideResources)
+  .filter(resource => filter ? resource.tags.includes(filter) : true));
+  
 
   return (
     <article>
       <div id='guide-header'>
         <h1>{guideData.name}</h1>
-        <small>{resourceCount} {resourceCount === 1 ? 'resource' : 'resources'}</small>
+        <small>{filteredResources.length === 1
+          ? '1 resource'
+          : `${filteredResources.length} resources`}
+        </small>
       </div>
 
-      <ReturnButton />
-      <ResourceTable />
+      <div id='guide-actions'>
+        <ReturnButton />
+        <FilterSelect tags={tags} setFilter={setFilter} />
+      </div>
+      
+      <ResourceTable resources={filteredResources} />
     </article>
   );
 }
